44b7c3540ee16c7aa1d0a1fa34ccd6d163623809
hreidar17 <hreidar17@ru.is>
2018-10-08 19:27:07 +0000

    new function added, param is a number array and outputs the total sum of its integers

--- src/calculator.js
+++ src/calculator.js
@@ -3,16 +3,22 @@ function add (numbers){
 		return 0
 	}
 	if(numbers.includes(",")){
-        var numberArr = numbers.split(",");
-        var total = 0;
-        for(var i = 0 ; i < numberArr.length ; i++){
-            total += parseInt(numberArr[i])
-        }
-        return total;
+        var arr = numbers.split(",");
+
+        return numberSum(arr);
     }
     else{
 	    return parseInt(numbers);
 	}
+}
+
+function numberSum(numberArr){
+    var total = 0;
+
+    for(var i = 0; i < numberArr.length; i++){
+        total += parseInt(numberArr[i]);
+    }
+    return total;
 }
 
 module.exports = add;
