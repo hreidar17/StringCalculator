3c1fcf47255b58853db725b29d2c248ef8487cec
hreidar17 <hreidar17@ru.is>
2018-10-08 17:49:58 +0000

    testing file edited, can parse either no input or a single int

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,11 @@
 function add (number){
-	return 0;
+    if(number == ""){
+		return 0
+	}
+	else{
+		return parseInt(number);
+	}
 }
 
 module.exports = add;
+
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,5 +4,9 @@ it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
 
+it("should return one number when string input is only one number", () => {
+	expect(add("5")).toBe(5);
+});
+
 
 
